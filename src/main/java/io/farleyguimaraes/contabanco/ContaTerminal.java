4b0e4b6078101b0d763ceafb60ca6aa4076cbaa3
Farley Guimarães <farley_guimaraes@hotmail.com>
2024-05-13 15:07:25 -0300

    finished

--- src/main/java/io/farleyguimaraes/contabanco/ContaTerminal.java
+++ src/main/java/io/farleyguimaraes/contabanco/ContaTerminal.java
@@ -2,4 +2,53 @@ package io.farleyguimaraes.contabanco;
 
 public class ContaTerminal {
 
+    private int numeroConta;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+    
+    
+    ContaTerminal(String nomeCliente, String agencia, int numeroConta, double saldo){
+        this.nomeCliente = nomeCliente;
+        this.agencia = agencia;
+        this.numeroConta = numeroConta;
+        this.saldo = saldo;
+    }
+    
+    public int getNumeroConta() {
+        return numeroConta;
+    }
+    
+    public String getAgencia() {
+        return agencia;
+    }
+    
+    public String getNomeCliente() {
+        return nomeCliente;
+    }
+    
+    public void alterarNomeCliente(String novoNomeCliente) {
+        this.nomeCliente = novoNomeCliente;
+    }
+    
+    public double getSaldo() {
+        return saldo;
+    }
+    
+    public void saque(int valor){
+        if(valor <= this.saldo){
+            this.saldo -= valor;
+        } else {
+            System.out.println("Saque não efetuado: Saldo insuficiente!");
+        }
+    }
+
+    public void deposito(int valor){
+        if(valor > 0){
+            this.saldo += valor;
+        } else {
+            System.out.println("Deposito não efetuado: Valor inválido!");
+        }
+    }
+    
 }
--- src/main/java/io/farleyguimaraes/contabanco/Main.java
+++ src/main/java/io/farleyguimaraes/contabanco/Main.java
@@ -1,7 +1,27 @@
 package io.farleyguimaraes.contabanco;
 
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.print("informe o nome do cliente: ");
+        String nomeCliente = scanner.next();
+        
+        System.out.print("Informe a agencia: ");
+        String agencia = scanner.next();
+
+        System.out.print("Informe o número da conta: ");
+        int numeroConta = scanner.nextInt();
+
+        System.out.print("Informe o saldo: ");
+        double saldo = scanner.nextDouble();
+        
+        ContaTerminal contaTerminal = new ContaTerminal(nomeCliente, agencia, numeroConta, saldo);
+        
+
+        System.out.println(String.format("Olá %s, obrigado por criar uma conta em nosso banco, sua agência é %s, conta %d e seu saldo %.2f já está disponível para saque",contaTerminal.getNomeCliente(),contaTerminal.getAgencia(),contaTerminal.getNumeroConta(),contaTerminal.getSaldo()));
     }
 }
